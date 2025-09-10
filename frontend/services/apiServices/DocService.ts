398fa618dbaa88e2ea417e5d35b9df753c85b272
Osita <chrisjerry070@gmail.com>
2025-09-10 18:38:53 +0400

    included Document api service

---
+++ frontend/services/apiServices/DocService.ts
@@ -0,0 +1,19 @@
+import { filterByPayloadType } from "@/app/documentation/page";
+import { api } from "../axios";
+
+interface DocServiceType {
+    filter(payload: filterByPayloadType): Promise<any>;
+    delete (id: string): Promise<any>
+}
+
+export const DocServices: DocServiceType = {
+    filter: async(payload: filterByPayloadType) => {
+        const res = await api.post("document/filter-documents", payload);
+        return res.data.docs;
+    },
+
+    delete: async(id: string) => {
+        const res = await api.delete(`document/delete?documentIdStr=${id}`);
+        return res.data.allDocuments;
+    }
+}
