9b1c6a0764b6fe3a039f556100b320172d99299b
Osita <chrisjerry070@gmail.com>
2025-09-12 17:43:46 +0400

    Aded reset password service to the Authentication api service

--- frontend/services/apiServices/AuthService.ts
+++ frontend/services/apiServices/AuthService.ts
@@ -2,59 +2,63 @@ import { LoginModelType, SignupModelType } from "../../Interfaces";
 import { api } from "../axios";
 
 interface AuthServiceType {
-    completeSignup(payload: SignupModelType): Promise<any>;
-    signup(payload: SignupModelType): Promise<any>;
-    googleAuth(credential: string): Promise<any>;
-    login(payload: LoginModelType ): Promise<any>;
-    logout(): Promise<any>;
-    sendOtp(email: string): Promise<any>;
-    verifyOtp(payload: any): Promise<any>;
-    verifyNewEmail(payload: any): Promise<any>;
+  completeSignup(payload: SignupModelType): Promise<any>;
+  signup(payload: SignupModelType): Promise<any>;
+  googleAuth(credential: string): Promise<any>;
+  login(payload: LoginModelType): Promise<any>;
+  logout(): Promise<any>;
+  sendOtp(email: string): Promise<any>;
+  verifyOtp(payload: any): Promise<any>;
+  verifyNewEmail(payload: any): Promise<any>;
+  resetPassword(payload: any): Promise<any>;
 }
 
-export const AuthService : AuthServiceType = {
-    
-    completeSignup: async(payload: SignupModelType) => {
-        await api.post("/auth/signup", payload);
-        await api.post(`/sendOTP?email=${encodeURIComponent(payload.email)}`);
-    },
+export const AuthService: AuthServiceType = {
+  completeSignup: async (payload: SignupModelType) => {
+    await api.post("/auth/signup", payload);
+    await api.post(`/sendOTP?email=${encodeURIComponent(payload.email)}`);
+  },
 
-    signup: async(payload: SignupModelType) => {
-        await api.post("/auth/signup", payload);
-    },
+  signup: async (payload: SignupModelType) => {
+    await api.post("/auth/signup", payload);
+  },
 
-    googleAuth: async(credential: string) => {
-        const res = await api.post(
-          "/auth/google",
-          { credential: credential },
-          { withCredentials: true }
-        );
+  googleAuth: async (credential: string) => {
+    const res = await api.post(
+      "/auth/google",
+      { credential: credential },
+      { withCredentials: true }
+    );
 
-        const token = res.data.accessToken;
-        return token;
-    },
+    const token = res.data.accessToken;
+    return token;
+  },
 
-    login: async(payload: LoginModelType) => {
-        const res = await api.post("/auth/login", payload, {
-        withCredentials: true,
-      });
-      const token = res.data.accessToken;
-      return token;
-    },
+  login: async (payload: LoginModelType) => {
+    const res = await api.post("/auth/login", payload, {
+      withCredentials: true,
+    });
+    const token = res.data.accessToken;
+    return token;
+  },
 
-    logout: async() => {
-        await api.post("auth/logout", {}, { withCredentials: true });
-    },
+  logout: async () => {
+    await api.post("/auth/logout", {}, { withCredentials: true });
+  },
 
-    sendOtp: async(email: string) => {
-        await api.post(`/sendOTP?email=${encodeURIComponent(email)}`);
-    },
+  sendOtp: async (email: string) => {
+    await api.post(`/sendOTP?email=${encodeURIComponent(email)}`);
+  },
 
-    verifyOtp: async(payload: any) => {
-        await api.post(`/verifyOtp`, payload);
-    },
+  verifyOtp: async (payload: any) => {
+    await api.post(`/verifyOtp`, payload);
+  },
 
-     verifyNewEmail: async(payload: any) => {
-        await api.post("/settings/verify-new-email", payload);
-     }
-}
+  verifyNewEmail: async (payload: any) => {
+    await api.post("/settings/verify-new-email", payload);
+  },
+
+  resetPassword: async (payload: any) => {
+    await api.post("/auth/reset-password", payload);
+  },
+};
